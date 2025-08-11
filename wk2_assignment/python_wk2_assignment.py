b06d81a4b4b45c9c064abf369536836a1ecbe3f1
shukrahyusuf11@gmail.com <shukrahyusuf11@gmail.com>
2025-08-11 15:44:35 +0100

    first commit

---
+++ wk2_assignment/python_wk2_assignment.py
@@ -0,0 +1,28 @@
+# Step 1: Creating an empty list
+my_list = []
+
+# Step 2: Append elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Step 3: Inseting 15 at the second position at index 1
+my_list.insert(1, 15)
+
+# Step 4: Extending the list with [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Step 5: Removing the last element
+my_list.pop()
+
+# Step 6: Sort the list in ascending order
+my_list.sort()
+
+# Step 7: Print the final list
+print(my_list)
+
+# Step 7: Find and print the index of the value 30
+index_of_30 = my_list.index(30)
+print("Sorted List:", my_list)
+print("Index of value 30:", index_of_30)
